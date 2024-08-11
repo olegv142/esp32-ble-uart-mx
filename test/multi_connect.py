7ce6f138445af952220ca95c01d2638121da8f95
Oleg Volkov <olegv142@gmail.com>
2024-08-11 16:12:14 +0300

    Move test class under if

--- test/multi_connect.py
+++ test/multi_connect.py
@@ -103,33 +103,34 @@ class MutliAdapter:
 	def on_peer_msg(self, idx, msg):
 		pass
 
-class TestMutliAdapter(MutliAdapter):
-	def __init__(self, port, peers=None):
-		super().__init__(port)
-		self.peers = peers
-	
-	def on_idle(self, version):
-		print('Idle, version ' + version.decode())
-		if self.peers:
-			self.connect(self.peers)
-
-	def on_connecting(self, idx):
-		print('Connecting to #%d' % idx)
-
-	def on_connected(self):
-		print('Connected')
-
-	def on_debug_msg(self, msg):
-		print('    ' + msg.decode())
-
-	def on_central_msg(self, msg):
-		print('[.] ' + msg.decode())
-		self.send_data(msg) # send it back
-
-	def on_peer_msg(self, idx, msg):
-		print(('[%d] ' % idx) + msg.decode())
 
 if __name__ == '__main__':
+	class TestMutliAdapter(MutliAdapter):
+		def __init__(self, port, peers=None):
+			super().__init__(port)
+			self.peers = peers
+
+		def on_idle(self, version):
+			print('Idle, version ' + version.decode())
+			if self.peers:
+				self.connect(self.peers)
+
+		def on_connecting(self, idx):
+			print('Connecting to #%d' % idx)
+
+		def on_connected(self):
+			print('Connected')
+
+		def on_debug_msg(self, msg):
+			print('    ' + msg.decode())
+
+		def on_central_msg(self, msg):
+			print('[.] ' + msg.decode())
+			self.send_data(msg) # send it back
+
+		def on_peer_msg(self, idx, msg):
+			print(('[%d] ' % idx) + msg.decode())
+
 	port  = sys.argv[1]
 	peers = [addr.encode() for addr in sys.argv[2:]]
 	with TestMutliAdapter(port, peers) as ad:
