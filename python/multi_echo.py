15b3ac6512d696763db916d0613059097737fe1a
Oleg Volkov <olegv142@gmail.com>
2024-08-12 21:45:59 +0300

    Add error counter to echo test

--- python/multi_echo.py
+++ python/multi_echo.py
@@ -15,14 +15,16 @@ sys.path.append('.')
 from ble_multi_adapter import MutliAdapter
 
 class EchoTest(MutliAdapter):
-	burst_len = 3
+	burst_len = 16
 	def __init__(self, port):
 		super().__init__(port)
-		self.last_sn = 0
+		self.last_tx_sn = 0
+		self.last_rx_sn = None
+		self.errors = 0
 
 	def send_msg(self):
-		self.last_sn += 1
-		self.send_data(str(self.last_sn).encode() + b'#')
+		self.last_tx_sn += 1
+		self.send_data(str(self.last_tx_sn).encode() + b'#')
 
 	def on_idle(self, version):
 		for _ in range(EchoTest.burst_len):
@@ -32,12 +34,21 @@ class EchoTest(MutliAdapter):
 		print('    ' + msg.decode())
 
 	def on_central_msg(self, msg):
-		print('[.] ' + msg.decode())
-
+		sn = int(msg[:-1])
+		print('[.] ' + msg.decode(), end='')
+		if self.last_rx_sn is not None and sn != self.last_rx_sn + 1:
+			print(' %u %u' % (self.last_rx_sn, sn))
+			self.errors += 1
+		else:
+			print()
+		self.last_rx_sn = sn
 
 if __name__ == '__main__':
 	with EchoTest(sys.argv[1]) as ad:
 		ad.reset()
-		while True:
-			ad.poll()
+		try:
+			while True:
+				ad.poll()
+		except KeyboardInterrupt:
+			print('%u errors' % ad.errors)
 
