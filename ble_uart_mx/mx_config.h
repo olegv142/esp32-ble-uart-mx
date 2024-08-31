ffb7ffc579021b20ac90d585285f15d03349bae2
Oleg Volkov <olegv142@gmail.com>
2024-08-31 18:16:30 +0300

    Yet another config

--- ble_uart_mx/mx_config.h
+++ ble_uart_mx/mx_config.h
@@ -1,19 +1,6 @@
 #pragma once
 
-// Here you can include the configuration of your choice
-#include "config/default.h"
-// .. for example
-
-// The following pair of config files is meant to be used for
-// creating point to point link with automatic connect.
-// The link is using simplified protocol with only data messages.
-// #include "config/simple_master.h"
-// #include "config/simple_slave.h"
-
-// The following configurations are meant to be used for testing
-// #include "config/echo.h"
-// #include "config/echo_master.h"
-// #include "config/uptime.h"
+#include "user_config.h"
 
 //
 // Miscellaneous settings
