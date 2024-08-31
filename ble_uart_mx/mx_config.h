8e7fe4defb635753a15af4a4a78dd65764df1645
Oleg Volkov <olegv142@gmail.com>
2024-08-31 14:14:59 +0300

    Shorten config file names

--- ble_uart_mx/mx_config.h
+++ ble_uart_mx/mx_config.h
@@ -1,19 +1,19 @@
 #pragma once
 
 // Here you can include the configuration of your choice
-#include "config/default_config.h"
+#include "config/default.h"
 // .. for example
 
 // The following pair of config files is meant to be used for
 // creating point to point link with automatic connect.
 // The link is using simplified protocol with only data messages.
-// #include "config/simple_master_config.h"
-// #include "config/simple_slave_config.h"
+// #include "config/simple_master.h"
+// #include "config/simple_slave.h"
 
 // The following configurations are meant to be used for testing
-// #include "config/echo_config.h"
-// #include "config/echo_master_config.h"
-// #include "config/uptime_config.h"
+// #include "config/echo.h"
+// #include "config/echo_master.h"
+// #include "config/uptime.h"
 
 //
 // Miscellaneous settings
