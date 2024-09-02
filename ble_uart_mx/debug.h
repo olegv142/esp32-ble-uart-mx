4d7746867e2fc5303dfcfa69ed5f4da6c5be3c33
Oleg Volkov <olegv142@gmail.com>
2024-09-02 18:53:52 +0300

    Cosmetic changes

--- ble_uart_mx/debug.h
+++ ble_uart_mx/debug.h
@@ -4,5 +4,7 @@
 
 void fatal(const char* what);
 
+#define LINE_STRING STRINGIZE(__LINE__)
+
 #define BUG() do { fatal("BUG at line " LINE_STRING); } while (0)
 #define BUG_ON(cond) do { if (cond) BUG(); } while (0)
--- ble_uart_mx/util.h
+++ ble_uart_mx/util.h
@@ -2,4 +2,3 @@
 
 #define STRINGIZE(x) STRINGIZE2(x)
 #define STRINGIZE2(x) #x
-#define LINE_STRING STRINGIZE(__LINE__)
