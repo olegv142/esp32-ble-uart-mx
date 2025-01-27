78bdd884b3987cc3d6533a817ae9e796a0a002c6
Oleg Volkov <olegv142@gmail.com>
2025-01-27 19:37:11 +0300

    Reduce the number of configs

--- ble_uart_mx/user_config.h
+++ ble_uart_mx/user_config.h
@@ -3,10 +3,8 @@
 // Here you can include the configuration of your choice
 // .. for example
 
-// Multirole adapter
-// #include "config/multirole.h"
 // Peripheral device with USB interface
-#include "config/peripheral.h"
+#include "config/usb_peripheral.h"
 // USB key as central device
 // #include "config/usb_key_central.h"
 
@@ -15,8 +13,3 @@
 // The link is using simplified protocol with only data messages.
 // #include "config/simple_master.h"
 // #include "config/simple_slave.h"
-
-// The following configurations are meant to be used for testing
-// #include "config/echo.h"
-// #include "config/echo_master.h"
-// #include "config/uptime.h"
