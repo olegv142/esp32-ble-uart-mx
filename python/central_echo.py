7ff343479c47944a91b9939bb80b2f3e6e83bd2d
Oleg Volkov <olegv142@gmail.com>
2025-03-13 00:07:44 +0300

    Fix central_echo.py to use binary data

--- python/central_echo.py
+++ python/central_echo.py
@@ -5,7 +5,7 @@ Using USB key to connect to the peripheral and echo back all received messages
 from ble_multi_adapter import MutliAdapter, find_port, PARITY_NONE
 import sys
 
-class UsbKeyEcho(MutliAdapter):
+class CentralEcho(MutliAdapter):
     parity = PARITY_NONE
     rtscts = False
 
@@ -28,11 +28,12 @@ class UsbKeyEcho(MutliAdapter):
     def on_peer_msg(self, idx, msg):
         print('[%d] %s' % (idx, msg))
         if msg:
-            self.send_data_to(idx, msg)
+            self.send_data_to(idx, msg, binary=True)
             self.msg_cnt += 1
 
 if __name__ == '__main__':
-    port = find_port(0x1a86, 0x55d3)
+    # port = find_port(0x1a86, 0x55d3) # USB key
+    port = find_port(0x303a, 0x1001) # USB CDC
     if not port:
         print('Adapter not found')
         sys.exit(-1)
@@ -41,12 +42,12 @@ if __name__ == '__main__':
         print('Target address(es) required')
         sys.exit(1)
 
-    with UsbKeyEcho(port, [p.encode() for p in sys.argv[1:]]) as ad:
+    with CentralEcho(port, [p.encode() for p in sys.argv[1:]]) as ad:
         ad.reset()
         try:
             while True:
                 ad.communicate()
         except KeyboardInterrupt:
-            print('%u messages received, %u parse errors' % (
-                ad.msg_cnt, ad.parse_errors
+            print('%u messages received, %d serial frames lost, %d parse errors' % (
+                ad.msg_cnt, ad.lost_frames, ad.parse_errors
             ))
