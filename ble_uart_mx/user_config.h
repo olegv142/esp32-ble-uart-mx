68270fe455f68183c9865491861465fcc3944f84
Oleg Volkov <olegv142@gmail.com>
2025-03-12 23:33:26 +0300

    Using usb_hidden.h by default

--- ble_uart_mx/user_config.h
+++ ble_uart_mx/user_config.h
@@ -4,14 +4,16 @@
 // .. for example
 
 // Peripheral device with USB interface
-//#include "config/usb_peripheral.h"
+// #include "config/usb_peripheral.h"
+// Device with USB interface having hidden peripheral role
+#include "config/usb_hidden.h"
+
 // USB key as central device
 // #include "config/usb_key_central.h"
 // USB key dual role
 // #include "config/usb_key.h"
 // USB key with hidden peripheral role
-#include "es_key.h"
-#include "config/usb_key_hidden.h"
+// #include "config/usb_key_hidden.h"
 
 // Serial adapter
 // #include "config/ble_serial.h"
