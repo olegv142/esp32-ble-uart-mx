e44efb37abc1eda56e34b36d44bb0114a369d28b
Oleg Volkov <olegv142@gmail.com>
2025-05-28 23:18:59 +0300

    RC522 project copied from peripheral template

---
+++ examples/ble_rc522/user_config.h
@@ -0,0 +1,33 @@
+#pragma once
+
+// Include any additional header before device config
+// for example secret key for authentication
+// #include "es_key.h"
+
+// Here you can include the configuration of your choice
+// .. for example
+
+// Peripheral device with USB interface
+#include "config/usb_peripheral_c3core.h"
+// Device with USB interface having hidden peripheral role
+// #include "config/usb_hidden.h"
+// Device with USB interface having only hidden peripheral role
+// #include "config/usb_peripheral_hidden.h"
+
+// USB key as central device
+// #include "config/usb_key_central.h"
+// USB key dual role
+// #include "config/usb_key.h"
+// USB key with hidden peripheral role
+// #include "config/usb_key_hidden.h"
+
+// Serial adapter
+// #include "config/ble_serial.h"
+// Serial adapter as central device only
+// #include "config/ble_serial_central.h"
+
+// The following pair of config files is meant to be used for
+// creating point to point link with automatic connect.
+// The link is using simplified protocol with only data messages.
+// #include "config/simple_master.h"
+// #include "config/simple_slave.h"
